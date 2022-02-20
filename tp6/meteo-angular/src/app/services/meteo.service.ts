4d1c21acb0a34dc1cf42378a0bd37da69e965488
Houssem Eddine Badraoui <34254609+Badraoui@users.noreply.github.com>
2022-02-20 19:43:16 +0100

    a

--- tp6/meteo-angular/src/app/services/meteo.service.ts
+++ tp6/meteo-angular/src/app/services/meteo.service.ts
@@ -9,7 +9,8 @@ export class MeteoService {
   getMeteo(name: string): Promise<any> {
     console.log('from service', name);
 
-    return fetch('https://api.openweathermap.org/data/2.5/weather/?q=' + name + '&units=metric&lang=fr&appid=cf16761d3c10ad23572e697143cac7b5')
+    return fetch('https://api.openweathermap.org/data/2.5/weather/?q=' + name + '&units=metric&lang=fr&appid=0ada432b59deb9716c357092c5f79be6')
+
       .then(function (response) {
         return response.json();
       })
